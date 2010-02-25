e4a06122b4e686a7178751f6ad0c8f0dd00cc0be
Rotem Yaari <vmalloc@gmail.com>
2010-02-25 08:39:59 +0200

    modify tests to comply with git 1.7.0 behavior of denying push to currently checked out branch

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -15,32 +15,41 @@ class TestWorkflows(unittest.TestCase):
                           utils.get_repo_contents(repo2))
 
     def testCloneModifyPush(self):
-        new_repo = LocalRepository(utils.get_temporary_location())
+        new_repo = LocalRepository(utils.get_temporary_location())        
         new_repo.clone(self.repo)
+        #checkout a different branch to comply with git 1.7.0
+        prev_branch = self.repo.getCurrentBranch()
+        self.repo.checkout(self.repo.createBranch('temp'))        
         self.assertReposEqual(self.repo, new_repo)
         utils.commit_change(new_repo)
         new_repo.push()
-        self.repo.resetHard()
+        self.repo.checkout(prev_branch)        
         self.assertReposEqual(self.repo, new_repo)        
         utils.delete_repository(new_repo)
     def testCloneRebaseModifyPush(self):
         new_repo = LocalRepository(utils.get_temporary_location())
         new_repo.clone(self.repo)
+        prev_branch = self.repo.getCurrentBranch()
+        #checkout a different branch to comply with git 1.7.0
+        self.repo.checkout(self.repo.createBranch('temp'))
         self.assertReposEqual(self.repo, new_repo)
         utils.commit_change(self.repo)
         utils.commit_change(new_repo)
         new_repo.fetch()
         new_repo.rebase('origin/master')
         new_repo.push()
-        self.repo.resetHard()
+        self.repo.checkout(prev_branch)
         self.assertReposEqual(self.repo, new_repo)        
         utils.delete_repository(new_repo)
     def testCloneModifyPushToBranch(self):
         new_repo = clone(self.repo, utils.get_temporary_location())
+        prev_branch = self.repo.getCurrentBranch()
+        self.repo.checkout(self.repo.createBranch('temp'))
         branch = new_repo.createBranch('work')
         new_repo.checkout(branch)
         utils.commit_change(new_repo)
         new_repo.push(self.repo, fromBranch=branch, toBranch='work')
+        self.repo.checkout(prev_branch)
         self.assertTrue(self.repo.getBranchByName('work').getHead() == new_repo.getHead())
 
 if __name__ == '__main__':
